a8afe01275b97d57c2e27daf1c3ee3f47b271d52
mona0403 <119569445+mona0403@users.noreply.github.com>
2023-03-09 00:43:48 +0530

    Add files via upload

---
+++ Coffee.java
@@ -0,0 +1,33 @@
+package coffee.entity;
+
+public class Coffee {
+	private String coffeeType;
+	private Double price;
+	
+	
+	
+	public Coffee(String coffeeType, Double price) {
+		super();
+		this.coffeeType = coffeeType;
+		this.price = price;
+	}
+	public String getCoffeeType() {
+		return coffeeType;
+	}
+	public void setCoffeeType(String coffeeType) {
+		this.coffeeType = coffeeType;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return "Coffee [coffeeType=" + coffeeType + ", price=" + price + "]";
+	}
+	
+	
+
+}
